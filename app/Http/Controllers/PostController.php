ba9418c3da201efea76f0538932ae1b609b4f843
daomapsieucap <bichdao.chau@gmail.com>
2021-09-13 21:38:25 +0700

    Section 6 - Search: Turn route stuff into Controller

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller{
+    public function index(){
+        return view('home', [
+            'posts'      => $this->getPosts(),
+            'categories' => Category::all()
+        ]);
+    }
+
+    public function show(Post $post){
+        return view('post', ['post' => $post]);
+    }
+
+    protected function getPosts(){
+        $posts = Post::latest();
+        if(request('search')){
+            $posts->where('title', 'like', '%' . request('search') . '%')
+                  ->orwhere('body', 'like', '%' . request('search') . '%');
+        }
+
+        return $posts->get();
+    }
+}
